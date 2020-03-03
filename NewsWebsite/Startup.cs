ea090968c08516edefb4c2fdd5a69cd6540308eb
MitkoZ <mitkoz15@abv.bg>
2020-03-03 23:48:33 +0200

    WIP

--- NewsWebsite/Startup.cs
+++ NewsWebsite/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,11 +8,13 @@ using DataAccess;
 using DataAccess.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Repositories;
 using Repositories.Interfaces;
@@ -114,6 +117,14 @@ namespace NewsWebsite
             }
 
             app.UseHttpsRedirection();
+            app.UseFileServer(new FileServerOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath = new PathString("/node_modules"),
+                EnableDirectoryBrowsing = true
+            });
+
+
             app.UseStaticFiles();
             app.UseAuthentication();
 
