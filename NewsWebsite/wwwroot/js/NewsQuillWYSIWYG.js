9a937469e2b108f2a3c1d6852daa13aa94f1e358
MitkoZ <mitkoz15@abv.bg>
2020-03-14 02:15:08 +0200

    Convert QuillJs from functional programming to OOP

--- NewsWebsite/wwwroot/js/NewsQuillWYSIWYG.js
+++ NewsWebsite/wwwroot/js/NewsQuillWYSIWYG.js
@@ -1,48 +1,76 @@
-﻿let quill = new Quill('#editor-container', {
-    modules: {
-        toolbar: [
-            [{ 'font': [] }],
-            [{ 'indent': '-1' }, { 'indent': '+1' }],
-            [{ 'direction': 'rtl' }],
-            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-            [{ 'color': [] }, { 'background': [] }],
-            [{ 'align': [] }],
-            ['bold', 'italic', 'underline', 'strike'],
-            ['link', 'blockquote', 'image', 'video'],
-            [{ list: 'ordered' }, { list: 'bullet' }],
-            ['clean']
-        ]
-    },
-    placeholder: 'Write your news...',
-    theme: 'snow'
-});
+﻿let WYSIWYGInstance; // if someone wants to reuse the previously created instance
 
-let form = document.getElementById("news-form");
-form.onsubmit = function () {
-    // Populate hidden form input on submit
-    populateHiddenInputWithQuillData();
-    trimQuillInput();
+function WYSIWYG(containerId, isDisabled) {
 
-    console.log("Submitted", $(form).serialize(), $(form).serializeArray());
-    return true;
-};
-
-function populateHiddenInputWithQuillData() {
-    let content = document.querySelector('input[name=Content]');
-    content.value = JSON.stringify(quill.getContents());
-}
-
-function populateVisibleContainerWithQuillData(data) {
-    quill.setContents(data);
-}
-
-function getQuillLength() {
-    return quill.getLength(); // Retrieves the length of the editor contents. Note even when Quill is empty, there is still a blank line represented by ‘\n’, so getLength will return 1. (source: https://quilljs.com/docs/api/#getlength)
-}
-
-function trimQuillInput() {
-    let content = document.querySelector('input[name=Content]');
-    if (getQuillLength() == 1) {
-        content.value = null;
+    // private functions
+    function getQuillLength() {
+        return quill.getLength(); // Retrieves the length of the editor contents. Note even when Quill is empty, there is still a blank line represented by ‘\n’, so getLength will return 1. (source: https://quilljs.com/docs/api/#getlength)
     }
+
+    function trimQuillInput(hiddenInputFilter) {
+        let content = document.querySelector(hiddenInputFilter);
+        if (getQuillLength() == 1) {
+            content.value = null;
+        }
+    }
+
+    // public functions declaration to achieve OOP style rather than functional programming
+    this.getQuill = function (isDisabled) {
+        if (isDisabled) {
+            return new Quill(containerId, {
+                modules: {
+                    toolbar: false
+                },
+                theme: "snow"
+            });
+            quill.disable();
+        }
+        else {
+            return new Quill(containerId, {
+                modules: {
+                    toolbar: [
+                        [{ 'font': [] }],
+                        [{ 'indent': '-1' }, { 'indent': '+1' }],
+                        [{ 'direction': 'rtl' }],
+                        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                        [{ 'color': [] }, { 'background': [] }],
+                        [{ 'align': [] }],
+                        ['bold', 'italic', 'underline', 'strike'],
+                        ['link', 'blockquote', 'image', 'video'],
+                        [{ list: 'ordered' }, { list: 'bullet' }],
+                        ['clean']
+                    ]
+                },
+                placeholder: 'Write your news...',
+                theme: 'snow'
+            });
+        }
+
+    };
+
+    this.populateInputWithWYSIWYGData = function (hiddenInputFilter) {
+        let content = document.querySelector(hiddenInputFilter);
+        content.value = JSON.stringify(quill.getContents());
+    }
+
+    this.populateContainerWithWYSIWYGData = function (data) {
+        quill.setContents(data);
+    }
+
+    this.attachOnSubmitWYSIWYGHandler = function (formId, hiddenInputFilter) {
+        let form = document.getElementById(formId);
+        let that = this;
+        form.onsubmit = function () {
+            // Populate hidden form input on submit
+            that.populateInputWithWYSIWYGData(hiddenInputFilter);
+            trimQuillInput(hiddenInputFilter);
+
+            return true;
+        };
+    }
+
+
+    // You should place your after instantiation logic here
+    let quill = this.getQuill(isDisabled);
+    WYSIWYGInstance = this;
 }
