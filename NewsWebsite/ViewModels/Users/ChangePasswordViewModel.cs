2de4b159728725842e934338a65744aef493418c
MitkoZ <mitkoz15@abv.bg>
2020-04-21 20:31:25 +0300

    feature/AllUsersListing-and-ChangePassword-by-admin (#7)
    Add feature - Admin is able to see a listing of all the users (with pagination, sort and filter). Add feature - Admin is able to change password of other users.

---
+++ NewsWebsite/ViewModels/Users/ChangePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewsWebsite.ViewModels.Users
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [HiddenInput]
+        public string UserId { get; set; }
+        public string Username { get; set; }
+    }
+}
--- NewsWebsite/ViewModels/Users/GetUserViewModel.cs
+++ NewsWebsite/ViewModels/Users/GetUserViewModel.cs
@@ -3,7 +3,7 @@
     public class GetUserViewModel
     {
         public string Id { get; set; }
-        public string Fullname { get; set; } //don't rename it, since we can't specify mappings in the front-end for it
-        public string Email { get; set; }
+        public string Fullname { get; set; } // don't rename it, since we can't specify mappings in the jquery-comments front-end library (searchUsers ajax request) for it
+        public string Email { get; set; } // don't rename it, since we can't specify mappings in the jquery-comments front-end library (searchUsers ajax request) for it
     }
 }
